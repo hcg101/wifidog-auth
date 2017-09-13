085d4929603cadf5edd838eb743190ec2456385a
Lukin <luxiansheng@mogoroom.com>
2017-09-13 10:14:27 +0800

    add files

--- tpl/login_ok.php
+++ tpl/login_ok.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <meta http-equiv="refresh" content="0;url=<?php echo $redir?>">
+    <meta http-equiv="refresh" content="3;url=<?php echo $redir?>">
     <title>WIFI Login Success</title>
     <style>
         body {
@@ -11,7 +11,9 @@
         }
     </style>
     <script>
-        window.location.replace('<?php echo $redir?>');
+        window.setTimeout(function () {
+            window.location.replace('<?php echo $redir?>');
+        },3000);
     </script>
 </head>
 <body>

a2e32649568673c693ff372a3256fbcfffe27a43
Lukin <luxiansheng@mogoroom.com>
2017-09-13 11:19:17 +0800

    add files

--- tpl/portal.php
+++ tpl/portal.php
@@ -3,20 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <meta http-equiv="refresh" content="1;url=<?php echo $redir ?>">
+    <meta http-equiv="refresh" content="0;url=<?php echo $redir ?>">
     <title>WIFI Login Success</title>
-    <style>
-        body {
-            text-align: center;
-        }
-    </style>
     <script>
         window.setTimeout(function () {
             window.location.replace('<?php echo $redir?>');
-        }, 1000);
+        }, 10);
     </script>
 </head>
 <body>
-<pre><?php print_r($_GET); ?></pre>
+<h1>跳转中...</h1><br>
+<a href="<?php echo $redir ?>"><?php echo $redir ?></a>
 </body>
 </html>

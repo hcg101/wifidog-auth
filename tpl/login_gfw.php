7e5f26b17b27c1ea2d6198a36693530303ec514c
Lukin <luxiansheng@mogoroom.com>
2017-09-13 14:05:23 +0800

    add files

--- tpl/login_gfw.php
+++ tpl/login_gfw.php
@@ -8,15 +8,19 @@
         body {
             text-align: center;
         }
+        h1, p {
+            margin: 5px;
+            padding: 0;
+        }
     </style>
 </head>
 <body>
 <form action="/login" method="post">
     <input type="hidden" name="redir" value="<?php echo $_GET['redir'] ?>">
     <h1>自动翻墙WIFI</h1>
+    <p><button type="submit" style="width: 80%; height: 50px; font-size: larger; font-weight: bold">立即翻墙</button></p>
     <p><img src="/qrcode.jpg" width="100%"></p>
     <p>有任何问题，请联系微信</p>
-    <p><button type="submit" style="width: 80%; height: 50px; font-size: larger; font-weight: bold">立即翻墙</button></p>
 </form>
 </body>
 </html>
